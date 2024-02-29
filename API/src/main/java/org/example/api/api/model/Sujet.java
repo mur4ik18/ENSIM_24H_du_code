9e5cc0043681eaf7a629d505e79a1e0354a92c39
Colaaas <152861842+Colaaas@users.noreply.github.com>
2024-02-29 11:33:38 +0100

    Create Sujet.java
    attributs classe Sujet

---
+++ API/src/main/java/org/example/api/api/model/Sujet.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Sujet {
+    private String titre;
+    private String explicationDetail;
+    private String porteur;
+    private List<Equipe> equipesInscrites;
+    private String domaine;
+    private String technologie;
+    
+}
