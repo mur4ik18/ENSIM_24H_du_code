5661c4cd6a2b63ef50725000f1beece5d9cd875c
Colaaas <152861842+Colaaas@users.noreply.github.com>
2024-02-29 11:30:54 +0100

    Create Equipe.java
    attributs classe Equipe

---
+++ API/src/main/java/org/example/api/api/model/Equipe.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Equipe {
+    private String nom;
+    private String nomPartenaireSponsor;
+    private String image;
+    private String motDePasse;
+    private List<Utilisateur> listeMembres;
+    private int paiement;
+    private int annee;
+    private String sujet;
+    private String lienWeb;
+    private String domaine;
+    private String type;
+    
+}
